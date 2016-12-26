16688752a855e0dbdb1a67133d667392f010d644
chotu24 <anandk.kumar506@gmail.com>
2016-12-25 21:15:11 -0500

    Finalizing app with appModule making declarations

--- Angular-Start/app/app.component.ts
+++ Angular-Start/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
-        <pm-products></pm-products>
-        <test-selector></test-selector>
+        <div><h1>Angular2: Getting Started {{pageTitle}}</h1>
+            <pm-products> </pm-products>
+        </div>
     `
 })
 
 export class AppComponent {
-
+    pageTitle: string = 'Anand Kumar Sant';
 }
--- Angular-Start/app/products/product-filter.pipes.ts
+++ Angular-Start/app/products/product-filter.pipes.ts
@@ -1,14 +1,28 @@
-import { PipeTransform, Pipe } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
+
 import { IProduct } from './product';
 
 @Pipe({
-    name: 'productsFilter'
+    name: 'productFilter'
 })
-
-export class ProductFilterPipe implements PipeTransform {
-        transform(value: IProduct[], filterBy: string): IProduct[] {
-
-            filterBy = filterBy ? filterBy.toLocaleLowerCase(): null;
-        return filterBy ? value.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy)!== -1 ) : value;
+export class ProductFilterPipe implements PipeTransform{
+    transform(value: IProduct[], filterBy: string):IProduct[] {
+        filterBy = filterBy ? filterBy.toLocaleLowerCase(): null;
+        return filterBy ? value.filter((product: IProduct) => 
+        product.productName.toLocaleLowerCase().indexOf(filterBy)!== -1) : value;
     }
 }
+// import { PipeTransform, Pipe } from '@angular/core';
+// import { IProduct } from './product';
+
+// @Pipe({
+//     name: 'productsFilter'
+// })
+
+// export class ProductFilterPipe implements PipeTransform {
+//         transform(value: IProduct[], filterBy: string): IProduct[] {
+
+//             filterBy = filterBy ? filterBy.toLocaleLowerCase(): null;
+//         return filterBy ? value.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy)!== -1 ) : value;
+//     }
+// }
